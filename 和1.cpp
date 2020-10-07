c55875d6f3fa8e15c290e6f17f5ca725a82b0d2b
Hoppy321 <72080096+Hoppy321@users.noreply.github.com>
2020-10-07 18:02:18 +0800

    Add files via upload

---
+++ 和1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int a,b,c; 
+	cout<<"2numbers?(空格隔开两数)";
+	cin>>a>>b;
+	c= a+b;
+	cout<<"两数的和为:"<<c;
+	return 0;
+ } 
---
+++ 和2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout<<"a=";
+	cin>>a;
+	cout<<"b=";
+	cin>>b;
+	int c;
+	c=a+b;
+	cout<<a<<"与"<<b<<"的和是"<<c<<'\n';
+	system("pause");
+	return 0; 
+	
+ } 
